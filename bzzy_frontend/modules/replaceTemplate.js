635a8d8c46728fa5d32de84df0967c7cb5748c4a
Tristan Bower <tristanbower@Tristans-MacBook-Pro.local>
2022-01-29 15:52:35 -0500

    committing front end code

--- bzzy_frontend/modules/replaceTemplate.js
+++ bzzy_frontend/modules/replaceTemplate.js
@@ -1,11 +1,11 @@
 module.exports = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%PRICE%}/g, product.ratio);
+  output = output.replace(/{%NUTRIENTS%}/g, product.saftey);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%FROM%}/g, product.address);
   output = output.replace(/{%ID%}/g, product.id);
 
   if (!product.lowPopulation)
