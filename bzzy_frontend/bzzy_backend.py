c9d03f1cf71b1ea16a06ee19462c03252a01fb7e
Evan P <evan@tomorrow.local>
2022-01-29 20:22:04 -0500

    backend almost finished

--- bzzy_frontend/bzzy_backend.py
+++ bzzy_frontend/bzzy_backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, request
 import os, datetime
 import sqlite3
+import json
 
 bzzy = Flask(__name__)
 
@@ -14,16 +15,35 @@ def recvdata():
 
             # DO SHIT
             cur_datetime = datetime.datetime.now()
+            node_id = int(request.form["id"])
+            clients = int(request.form["clients"])
             date = str(cur_datetime.year)+'-'+str(cur_datetime.month)+'-'+str(cur_datetime.day)
             time = str(cur_datetime.hour)+':'+str(cur_datetime.minute)+':'+str(cur_datetime.second)
-            sql_data = "'"+request.form["id"]+"','"+date+"','"+time+"',"+request.form["clients"]
+            sql_data = request.form["id"]+",'"+date+"','"+time+"',"+request.form["clients"]
             cur.execute('INSERT INTO data VALUES ('+sql_data+')')
 
             con.commit()
+        update_JSON(node_id, clients, '99', False)
         return ('', 204)
 
-def update_JSON():
-    pass
+def update_JSON(node_id, quantity, ratio, lowPop):
+    with open(FN_JSONDATA, 'r') as jsonfile:
+        data = json.load(jsonfile)
+        for item in data:
+            if item['id'] == node_id:
+                item['quantity'] = quantity
+                item['ratio'] = ratio
+                item['lowPopulation'] = lowPop
+                break
+            print('[!] Unknown ID:', node_id)
+    with open(FN_JSONDATA, 'w') as jsonfile:
+        json.dump(data, jsonfile)
+
+def parse_db(node_id):
+    with sqlite3.connect(FN_DATABASE) as con:
+        cur = con.cursor()
+        for row in cur.execute('SELECT * FROM data WHERE ID = '+str(node_id)):
+            print(row)
 
 if __name__ == '__main__':
     bzzy.run(debug=True,host='0.0.0.0',port=8080)
